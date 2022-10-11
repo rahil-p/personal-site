0cc283d68916637b458f7f1add3dfc5f30e0666f
Rahil Patel <37254995+rahil-p@users.noreply.github.com>
2022-10-10 21:11:00 -0700

    simplify heading style

--- services/site/src/lib/react/router/home/intro/IntroHeading.tsx
+++ services/site/src/lib/react/router/home/intro/IntroHeading.tsx
@@ -1,49 +1,33 @@
 import { css } from '@emotion/react';
 
-import BlobsGraphic from '../../../../../public/images/ui/blobs-a.svg';
-
 export default function IntroHeading() {
 	return (
-		<div
+		<hgroup
 			css={css`
-				position: relative;
 				margin-bottom: 1.25em;
 				font-size: clamp(2.25rem, 7.5vw, 5.75rem);
+				font-weight: 700;
 				line-height: 1;
 			`}
 		>
-			<hgroup
+			<span
 				css={css`
-					font-weight: 700;
+					display: block;
+					margin-bottom: 0.3em;
+					font-size: 0.65em;
 				`}
 			>
-				<span
-					css={css`
-						display: block;
-						margin-bottom: 0.3em;
-						font-size: 0.65em;
-					`}
-				>
-					Hello. I&apos;m
-				</span>
-				<h1
-					css={css`
-						position: relative;
-						left: -0.05em;
-						font-size: 1em;
-					`}
-				>
-					Rahil Patel
-				</h1>
-			</hgroup>
-			<BlobsGraphic
+				Hello. I&apos;m
+			</span>
+			<h1
 				css={css`
-					position: absolute;
-					top: 0.425em;
-					left: 1.565em;
-					height: 0.14em;
+					position: relative;
+					left: -0.05em;
+					font-size: 1em;
 				`}
-			/>
-		</div>
+			>
+				Rahil Patel
+			</h1>
+		</hgroup>
 	);
 }
